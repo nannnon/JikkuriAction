6f3417e854526604c611463d1719336787935c34
Koba <koba@dummy.com>
2022-09-20 23:22:55 +0900

    カメラがプレイヤーを追従するようにした

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _followTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void LateUpdate()
+    {
+        const int FreeAreaWidth = 7;
+        const int FreeAreaHeight = 7;
+
+        float diffX = _followTarget.transform.position.x - transform.position.x;
+        float diffY = _followTarget.transform.position.y - transform.position.y;
+
+        if (diffX < -FreeAreaWidth / 2f)
+        {
+            transform.position += new Vector3(diffX + FreeAreaWidth / 2f, 0, 0);
+        }
+        if (FreeAreaWidth / 2f < diffX)
+        {
+            transform.position += new Vector3(diffX - FreeAreaWidth / 2f, 0, 0);
+        }
+        if (diffY < -FreeAreaHeight / 2f)
+        {
+            transform.position += new Vector3(0, diffY + FreeAreaHeight / 2f, 0);
+        }
+        if (FreeAreaHeight / 2f < diffY)
+        {
+            transform.position += new Vector3(0, diffY - FreeAreaHeight / 2f, 0);
+        }
+    }
+}
